9de9e71f3cc4c26688a3cf0a9baf26f837de0499
stevmert <steven.mertens@ugent.be>
2022-04-02 16:50:46 +0200

    update to kodi 19

--- video.nba.leaguepass.sm/main.py
+++ video.nba.leaguepass.sm/main.py
@@ -1,11 +1,9 @@
-
-
-import xbmc, xbmcaddon
+import xbmc, xbmcaddon, xbmcvfs
 import sys, os
 
 
 my_addon = xbmcaddon.Addon('video.nba.leaguepass.sm')
-addon_dir = xbmc.translatePath(my_addon.getAddonInfo('path')).decode('utf-8')
+addon_dir = xbmcvfs.translatePath(my_addon.getAddonInfo('path'))
 
 sys.path.append(os.path.join(addon_dir, 'src'))
 
